e8cd335bec5e511100bd47c9a68e368fa0204334
Sunny Lee <33865037+CSJLIMO@users.noreply.github.com>
2020-09-26 00:12:19 +0800

    Update square.py

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -8,11 +8,11 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 @app.route('/square', methods=['POST'])
-def evaluate():
+def evaluate_square():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input");
-    result = inputValue * inputValue * inputValue
+    result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result);
 
