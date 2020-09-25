1f8ae4952e3b37677e8ec51e58cf8c443cddef40
Sunny Lee <33865037+CSJLIMO@users.noreply.github.com>
2020-09-26 01:45:43 +0800

    Add files via upload

---
+++ codeitsuisse/routes/sort.py
@@ -0,0 +1,20 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+    
+
+@app.route('/sort', methods=['POST'])
+def evaluate_sort():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    result= [1, 2, 3]
+    logging.info("My result :{}".format(result))
+    return jsonify(result);
+
+
+
