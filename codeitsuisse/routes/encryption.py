ea1c11375fc5101242720f44abae7c92a890c39e
Sunny Lee <33865037+CSJLIMO@users.noreply.github.com>
2020-09-26 00:29:46 +0800

    Add files via upload

---
+++ codeitsuisse/routes/encryption.py
@@ -0,0 +1,48 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+
+def get_ans(n, text):
+    new_text = ""
+    for ch in text:
+        if ch.isalnum():
+            new_text += ch.upper()
+    
+    ans = []
+    for i in range(len(new_text)):
+        ans.append(0)
+    
+    cnt = 0
+    for st in range(n):
+        cur = st
+        while cur < len(new_text):
+            ans[cur] = new_text[cnt]
+            cnt += 1
+            cur += n
+            
+    ret = ""
+    for ch in ans:
+        ret += ch        
+    
+    return ret
+    
+
+@app.route('/encryption', methods=['POST'])
+def evaluate_encryption():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    inp = data.get("input");
+    result = []
+    for test_case in inp:
+        result.append(get_ans(test_case["n"], test_case["text"]))
+    logging.info("My result :{}".format(result))
+    return json.dumps(result);
+
+
+
