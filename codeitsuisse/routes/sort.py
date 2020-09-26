6bdcbe37c906e597ed86d00a63e50713597e9ff6
Sunny Lee <33865037+CSJLIMO@users.noreply.github.com>
2020-09-26 08:55:54 +0800

    Update sort.py

--- codeitsuisse/routes/sort.py
+++ codeitsuisse/routes/sort.py
@@ -12,10 +12,19 @@ logger = logging.getLogger(__name__)
 def evaluate_sort():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    result = sorted(data)
+    
+    bkt = []
+    for i in range(20001): 
+        bkt.append(0)
+    for num in data:
+        bkt[num + 10000] += 1
+    result = []
+    for i in range(len(bkt)):
+        for j in range(bkt[i]):
+            result.append(i - 10000)
+            
     logging.info("My result :{}".format(result))
     return jsonify(result);
-
 
 
 
