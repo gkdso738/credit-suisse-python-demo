6f9aa5705540f6c00f17c3b206c6f379819ac594
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2020-09-26 13:10:12 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -6,7 +6,7 @@ import codeitsuisse.routes.sort
 import codeitsuisse.routes.salad_spree
 import codeitsuisse.routes.clean_floor
 import codeitsuisse.routes.fruitbasket
-
+import codeitsuisse.routes.intelligent_farming
 
 
 
