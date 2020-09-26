f4afdf85b80c200f292f5c2bfd5ccbe3bd8bb9fe
Sunny Lee <33865037+CSJLIMO@users.noreply.github.com>
2020-09-26 10:40:51 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -4,6 +4,7 @@ import codeitsuisse.routes.square
 import codeitsuisse.routes.encryption
 import codeitsuisse.routes.sort
 import codeitsuisse.routes.salad_spree
+import codeitsuisse.routes.clean_floor
 
 
 
